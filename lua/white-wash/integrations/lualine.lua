9c60a8d3b04d95be1e7e4843be942ebbdd08efd8
Clement <clrapin@proton.me>
2024-08-29 16:30:17 +0200

    Fixed test

--- lua/white-wash/integrations/lualine.lua
+++ lua/white-wash/integrations/lualine.lua
@@ -1,32 +1,49 @@
-return {
-  normal = {
-    a = {bg = #ffffff, fg = #ffffff, gui = 'bold'},
-    b = {bg = #ffffff, fg = #ffffff},
-    c = {bg = #ffffff, fg = #ffffff}
-  },
-  insert = {
+local colors = {
+  black        = '#282828',
+  white        = '#ebdbb2',
+  red          = '#fb4934',
+  green        = '#b8bb26',
+  blue         = '#83a598',
+  yellow       = '#fe8019',
+  gray         = '#a89984',
+  darkgray     = '#3c3836',
+  lightgray    = '#504945',
+  inactivegray = '#7c6f64',
+}
+
+return function()
+	local white_wash = {}
+
+  white_wash.normal = {
+    a = {bg = '#ffffff', fg = '#ffffff', gui = 'bold'},
+    b = {bg = '#ffffff', fg = '#ffffff'},
+    c = {bg = '#ffffff', fg = '#ffffff'}
+  }
+  white_wash.insert = {
     a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
     b = {bg = colors.lightgray, fg = colors.white},
     c = {bg = colors.lightgray, fg = colors.white}
-  },
-  visual = {
+  }
+  white_wash.visual = {
     a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
     b = {bg = colors.lightgray, fg = colors.white},
     c = {bg = colors.inactivegray, fg = colors.black}
-  },
-  replace = {
+  }
+  white_wash.replace = {
     a = {bg = colors.red, fg = colors.black, gui = 'bold'},
     b = {bg = colors.lightgray, fg = colors.white},
     c = {bg = colors.black, fg = colors.white}
-  },
-  command = {
+  }
+  white_wash.command = {
     a = {bg = colors.green, fg = colors.black, gui = 'bold'},
     b = {bg = colors.lightgray, fg = colors.white},
     c = {bg = colors.inactivegray, fg = colors.black}
-  },
-  inactive = {
+  }
+  white_wash.inactive = {
     a = {bg = colors.darkgray, fg = colors.gray, gui = 'bold'},
     b = {bg = colors.darkgray, fg = colors.gray},
     c = {bg = colors.darkgray, fg = colors.gray}
   }
-}
+
+	return white_wash
+end
