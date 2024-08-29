392f1fe935f367243e90b119c619da8a3cc76427
Clement <clrapin@proton.me>
2024-08-29 15:08:28 +0200

    Fixed lualine config

--- lua/lualine.lua
+++ lua/lualine.lua
@@ -1,5 +1,11 @@
-return require("lualine").setup({
-	options = {
-		theme = "ayu_light"
-	}
-})
+local config = function()
+	local lualine = require('lualine')
+
+	lualine.setup({
+		options = {
+			theme = 'ayu_light'
+		}
+	})
+end
+
+config()
